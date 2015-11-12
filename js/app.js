aad5504b07d6b28b5c0e79d8ebff3534de5cea09
tiwans <sukhman.2013@hotmail.com>
2015-11-12 15:07:36 -0800

    validation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 //Javascript for the Team challenge
 
 'use strict';
+// create angular app
+var validation = angular.module('validation', []);
+
+// create angular controller
+validation.controller('controller', function($scope) {
+
+    // function to submit the form after all validation has occurred            
+    $scope.submit = function() {
+    };
+
+});
