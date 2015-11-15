a0ff84ac4a35021c0e48e448dc8ad7d879acb05e
alex-ramos <aramos3@uw.edu>
2015-11-15 15:55:35 -0800

    adds functions to check birthdate and passwords

--- js/app.js
+++ js/app.js
@@ -10,9 +10,47 @@ validation.controller('MainCtrl', ["$scope", function($scope) {
     // function to submit the form after all validation has occurred            
     $scope.submitForm = function(isValid) {
     	// check to make sure the form is completely valid
-    	if(isValid) {
+    	if(isValid && checkBirthDate) {
     		alert("You have successfully submitted");
     	}
     };
 
+    //Checks entered bithdate against date 13 years ago
+    //Returns true if birthday is valid, false if birthday is not
+    $scope.checkBirthDate = function(){
+    	var bDate = Date.parse($scope.main.birthdate);
+    	var oldDate = new Date();
+    	//var currentD = new Date();
+    	oldDate.setFullYear(oldDate.getFullYear() - 13);
+    	//console.log(oldDate.toString());
+    	console.log(bDate.toString());
+    	if(bDate < oldDate){
+    		console.log(true);
+    	}else{
+	     	console.log(false);
+	    }
+    };
+
+
+    //Checks both password fields and if they match each other
+    //Returns true if match, false if different
+    $scope.confirmPassword = function(){
+    	var pwd1 = $scope.main.password;
+    	var pwd2 = $scope.main.cPassw;
+
+    	console.log(pwd1);
+    	console.log(pwd2);
+
+    	if(pwd1 === pwd2){
+    		console.log(true);
+    		return true;
+    	}
+    	else{
+    		console.log(false);
+    		return false;
+    	}
+    	
+    }
+
 }]);
+
